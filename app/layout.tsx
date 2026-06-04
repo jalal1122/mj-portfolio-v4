75c16f3ca07792ded693e1ff12a56fe04e5f8ea0
Muhammad Jalal <jk4350649@gmail.com>
2026-06-04 15:42:44 +0500

    change the favicon icon to custom

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,23 +21,14 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: site.title,
   description: site.description,
-  generator: "v0.app",
+  generator: "mjdevportfolio",
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/icon.png",
+        type: "image/png",
       },
     ],
-    apple: "/apple-icon.png",
   },
 };
 
